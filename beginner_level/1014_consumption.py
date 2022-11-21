cf8989063fd0d59e46fb993ae711b8c774e0a94e
Livia <liv.alvessantana@gmail.com>
2022-11-21 14:56:22 -0300

    ex 1014 adicionado

---
+++ beginner_level/1014_consumption.py
@@ -0,0 +1,7 @@
+# Calculate a car's average consumption being provided the total distance traveled 
+# (in Km) and the spent fuel total (in liters).
+
+a=int(input())
+b=float(input())
+
+print("%.3f km/l" % (float(a)/b))
